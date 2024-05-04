157ee51b7a54c0bd9d98fb3c8071b195780d2776
FeriaMaoMao <mauriciogonzalezferia@gmail.com>
2024-05-04 00:44:38 +0000

    :recycle: Refactor Module

--- iam-role-module.js
+++ iam-role-module.js
@@ -1,75 +1,41 @@
-// const cdk = require('aws-cdk-lib');
-// const iam = require('aws-cdk-lib/aws-iam');
-
-// const iamRoleModuleProps = {
-//   roleName: '',
-//   description: '',
-//   policyName: '',
-//   actions: [],
-//   resources: [],
-//   ServicePrincipal: []
-// };
-
-// // Parameter to IAM Role
-// class iamRoleModule extends cdk.Construct {
-//   constructor(scope, id, props) {
-//     super(scope, id);
-
-//     // Create Role with OIDC
-//     const role = new iam.Role(this, 'Role', {
-//       roleName: props.roleName, // Custom Role Name
-//       description: props.description, // Custom Description to Role
-//       assumedBy: new iam.ServicePrincipal(props.ServicePrincipal),
-//     });
-
-//     // Create Policy
-//     const policy = new iam.Policy(this, 'Policy', {
-//       policyName: props.policyName, // Custom Policy Name
-//       statements: [
-//         new iam.PolicyStatement({
-//           effect: iam.Effect.ALLOW,
-//           actions: props.actions, // Allowable Action
-//           resources: props.resources // Resource Allowed Access
-//         })
-//       ]
-//     });
-
-//     // Attaching the Policy to the OIDC Role
-//     role.attachInlinePolicy(policy);
-//   }
-// }
-
-// module.exports = { iamRoleModule };
-
-
 const cdk = require('aws-cdk-lib');
 const iam = require('aws-cdk-lib/aws-iam');
 
+const iamRoleModuleProps = {
+  roleName: '',
+  description: '',
+  policyName: '',
+  actions: [],
+  resources: [],
+  ServicePrincipal: []
+};
+
+// Parameter to IAM Role
 class iamRoleModule extends cdk.Construct {
   constructor(scope, id, props) {
     super(scope, id);
 
-    Object.entries(props.roles).forEach(([roleKey, roleProps]) => {
-      // Crear un nuevo rol para cada conjunto de propiedades
-      const role = new iam.Role(this, roleProps.name, {
-        roleName: roleProps.name,
-        description: roleProps.description,
-        assumedBy: new iam.ServicePrincipal(roleProps.ServicePrincipal),
-      });
-
-      const policy = new iam.Policy(this, `${roleProps.name}-Policy`, {
-        policyName: roleProps.policyName,
-        statements: [
-          new iam.PolicyStatement({
-            effect: iam.Effect.ALLOW,
-            actions: roleProps.actions,
-            resources: roleProps.resources
-          })
-        ]
-      });
-
-      role.attachInlinePolicy(policy);
+    // Create Role with OIDC
+    const role = new iam.Role(this, 'Role', {
+      roleName: props.roleName, // Custom Role Name
+      description: props.description, // Custom Description to Role
+      assumedBy: new iam.ServicePrincipal(props.ServicePrincipal), // Custom Assume Role
     });
+
+    // Create Policy
+    const policy = new iam.Policy(this, 'Policy', {
+      policyName: props.policyName, // Custom Policy Name
+      statements: [
+        new iam.PolicyStatement({
+          effect: iam.Effect.ALLOW,
+          actions: props.actions, // Allowable Action
+          resources: props.resources // Resource Allowed Access
+        })
+      ]
+    });
+
+    // Attaching the Policy to the OIDC Role
+    role.attachInlinePolicy(policy);
   }
 }
 
