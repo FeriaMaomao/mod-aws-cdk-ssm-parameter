56cdc3c28b2cbf63772bdb3e73fecc17d9d65bf7
FeriaMaoMao <mauriciogonzalezferia@gmail.com>
2024-05-04 17:40:57 +0000

    :recycle: Refector module and Package.json

--- iam-role-module.js
+++ iam-role-module.js
@@ -1,41 +1,34 @@
 const cdk = require('aws-cdk-lib');
 const iam = require('aws-cdk-lib/aws-iam');
+const { Construct } = require('constructs');
 
-const iamRoleModuleProps = {
-  roleName: '',
-  description: '',
-  policyName: '',
-  actions: [],
-  resources: [],
-  ServicePrincipal: []
-};
-
-// Parameter to IAM Role
-class iamRoleModule extends cdk.Construct {
+// Class to IAM Role
+class iamRoleModule extends Construct {
   constructor(scope, id, props) {
     super(scope, id);
 
-    // Create Role with OIDC
-    const role = new iam.Role(this, 'Role', {
-      roleName: props.roleName, // Custom Role Name
-      description: props.description, // Custom Description to Role
-      assumedBy: new iam.ServicePrincipal(props.ServicePrincipal), // Custom Assume Role
-    });
+    Object.entries(props.roles).forEach(([roleKey, roleProps]) => {
+      // Create Role for each properties
+      const role = new iam.Role(this, roleProps.name, {
+        roleName: roleProps.name,
+        description: roleProps.description,
+        assumedBy: new iam.ServicePrincipal(roleProps.ServicePrincipal),
+      });
 
-    // Create Policy
-    const policy = new iam.Policy(this, 'Policy', {
-      policyName: props.policyName, // Custom Policy Name
-      statements: [
-        new iam.PolicyStatement({
-          effect: iam.Effect.ALLOW,
-          actions: props.actions, // Allowable Action
-          resources: props.resources // Resource Allowed Access
-        })
-      ]
-    });
+      // Create Policy for each properties
+      const policy = new iam.Policy(this, `${roleProps.name}-Policy`, {
+        policyName: roleProps.policyName,
+        statements: [
+          new iam.PolicyStatement({
+            effect: iam.Effect.ALLOW,
+            actions: roleProps.actions,
+            resources: roleProps.resources
+          })
+        ]
+      });
 
-    // Attaching the Policy to the OIDC Role
-    role.attachInlinePolicy(policy);
+      role.attachInlinePolicy(policy);
+    });
   }
 }
 
